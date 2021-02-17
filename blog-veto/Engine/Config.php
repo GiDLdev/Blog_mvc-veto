ce9ef2aab705e99b60e04e9109ee47f731b9544a
GiDLdev <contact.gildas@free.fr>
2021-02-17 11:42:56 +0100

    misàjour config.php

--- blog-veto/Engine/Config.php
+++ blog-veto/Engine/Config.php
@@ -7,7 +7,7 @@ final class Config
     // Identifiants Database
     const
     DB_HOST = 'localhost',
-    DB_NAME = 'clinique_veterinaire_mvcnew',
+    DB_NAME = 'blog-veto',
     DB_USR = 'root',
     DB_PWD = 'root',
 
